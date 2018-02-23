2ae1689de0e80df5a94478cb89274f6b689a1851
Do Nhat Minh <mrordinaire@gmail.com>
2018-02-22 22:12:57 -0800

    fixed copying go packages

--- src/pgo/trans/PGoTranslator.java
+++ src/pgo/trans/PGoTranslator.java
@@ -57,8 +57,7 @@ public class PGoTranslator {
 
 	public void copyPackages(PGoOptions opts) throws IOException {
 		FileUtils.copyDirectory(new File("src/go/pgo"), new File(opts.buildDir + "/src/pgo"));
-		FileUtils.copyDirectory(new File("src/go/github.com/emirpasic"),
-				new File(opts.buildDir + "/src/github.com/emirpasic"));
+		FileUtils.copyDirectory(new File("src/go/github.com"), new File(opts.buildDir + "/src/github.com"));
 		FileUtils.copyDirectory(new File("src/go/pgonet"), new File(opts.buildDir + "/src/pgonet"));
 	}
 
